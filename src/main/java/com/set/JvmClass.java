109227a938aa40be1a3ca2bc0bcbf50329b9309f
zhanghuang <password>
2021-02-22 21:50:23 +0800

    jvm

---
+++ src/main/java/com/set/JvmClass.java
@@ -0,0 +1,13 @@
+package com.set;
+
+/**
+ * @author 97083
+ */
+public class JvmClass {
+
+    public static void main(String[] args) {
+        int a=2;
+        int b=3;
+        int c= a+b;
+    }
+}
